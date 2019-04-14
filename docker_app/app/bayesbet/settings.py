d5375fc428322d618846b009f8678177e151f220
Everett Robinson <evjrob@gmail.com>
2019-04-13 20:44:44 -0600

    Connect django to AWS RDS instance

--- docker_app/app/bayesbet/settings.py
+++ docker_app/app/bayesbet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from retrieve_db_creds import get_secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,14 +73,16 @@ WSGI_APPLICATION = 'bayesbet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+db_creds = get_secret()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'NAME': db_creds['DATABASE_NAME'],
+        'USER': db_creds['DATABASE_USER'],
+        'PASSWORD': db_creds['DATABASE_PASSWD'],
+        'HOST': db_creds['DATABASE_HOST'],
+        'PORT': int(db_creds['DATABASE_PORT']),
     }
 }
 
