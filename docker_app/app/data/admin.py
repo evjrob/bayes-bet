51637e9368ba9e74238b871b2923782a05e338dd
Everett Robinson <evjrob@gmail.com>
2019-08-04 11:11:18 -0600

    Remove currently unnecessary data models

--- docker_app/app/data/admin.py
+++ docker_app/app/data/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import Data
-
-admin.site.register(Data)
--- docker_app/app/data/views.py
+++ docker_app/app/data/views.py
@@ -10,6 +10,17 @@ def index(request, version='v1'):
 
 def games(request, version='v1', date=dt.date.today().strftime("%Y-%m-%d")):
     with connections['bayes_bet'].cursor() as cursor:
-        cursor.execute("SELECT DISTINCT game_pk FROM games WHERE game_date = %s;", [date])
-        rows = [item[0] for item in cursor.fetchall()]
+        query =  """SELECT DISTINCT game_pk, home_team_name, away_team_name 
+                    FROM games 
+                    LEFT JOIN 
+                        (SELECT team_id as home_team_id, team_name as home_team_name
+                        FROM teams) AS home_teams
+                    ON games.home_team_id = home_teams.home_team_id
+                    LEFT JOIN
+                        (SELECT team_id as away_team_id, team_name as away_team_name
+                        FROM teams) AS away_teams
+                    ON games.away_team_id = away_teams.away_team_id
+                    WHERE game_date = %s;"""
+        cursor.execute(query, [date])
+        rows = [{'game_pk':item[0], 'home_team':item[1], 'away_team':item[2]} for item in cursor.fetchall()]
         return JsonResponse({"data" : rows})
