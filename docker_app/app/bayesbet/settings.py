41e5a31fa60b5de112348d405666ca7a67ac2804
Everett Robinson <evjrob@gmail.com>
2019-08-05 10:08:11 -0600

    Include django-cors-headers

--- docker_app/app/bayesbet/settings.py
+++ docker_app/app/bayesbet/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'data.apps.DataConfig',
     'plots.apps.PlotsConfig',
 ]
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,3 +137,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CORS HEADERS
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    "https://everettsprojects.com",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "file://*"
+]
