11d5bafaa8f138a6d1f7e13cb87a0617a0e97b95
Everett Robinson <evjrob@gmail.com>
2019-12-15 13:55:55 -0700

    I don't think I understand django static files

--- front_end/app/bayesbet/settings.py
+++ front_end/app/bayesbet/settings.py
@@ -140,10 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+   "static"
 ]
 
 # CORS HEADERS
