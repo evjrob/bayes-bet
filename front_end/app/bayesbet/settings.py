c6ff68e1f595d4f5e57567258f89f57104468b3a
Everett Robinson <evjrob@gmail.com>
2019-12-24 10:29:56 -0700

    Finally figured out Django static files setup

--- front_end/app/bayesbet/settings.py
+++ front_end/app/bayesbet/settings.py
@@ -141,10 +141,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
-   "static"
+   "plots/static",
+   "bayesbet/static"
 ]
 
 # CORS HEADERS
