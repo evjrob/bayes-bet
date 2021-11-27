38f2b7802e9d14fec615640acaa960e4c5b3ea58
Everett Robinson <evjrob@gmail.com>
2021-11-27 15:57:10 -0700

    Fill out step function state machine with placeholder tasks

--- local_stack/terraform/stepfunction.tf
+++ local_stack/terraform/stepfunction.tf
@@ -19,26 +19,82 @@ resource "aws_sfn_state_machine" "bayesbet_nhl_sfn" {
           "job_id.$": "$$.Execution.Name"
         }
       },
-      "Next": "UpdatePreviousRecord"
+      "Next": "DailyUpdate"
     },
-    "UpdatePreviousRecord": {
-      "Type": "Task",
-      "Resource": "arn:aws:lambda:us-east-1:012345678912:function:function",
-      "Parameters": {
-        "league": "nhl",
-        "task": "update_previous_record",
-        "task_parameters": {
-          "bucket_name": "${aws_s3_bucket.bayesbet_pipeline_bucket.id}",
-          "pipeline_name": "${var.project}-main-${var.env}",
-          "job_id.$": "$$.Execution.Name",
-          "last_pred_date.$": "$.last_pred_date",
-          "season_start.$": "$.season_start"
+    "DailyUpdate": {
+      "Type": "Parallel",
+      "End": true,
+      "Branches": [
+        {
+          "StartAt": "UpdatePreviousRecord",
+          "States": {
+            "UpdatePreviousRecord": {
+              "Type": "Task",
+              "Resource": "arn:aws:lambda:us-east-1:012345678912:function:function",
+              "Parameters": {
+                "league": "nhl",
+                "task": "update_previous_record",
+                "task_parameters": {
+                  "bucket_name": "${aws_s3_bucket.bayesbet_pipeline_bucket.id}",
+                  "pipeline_name": "${var.project}-main-${var.env}",
+                  "job_id.$": "$$.Execution.Name",
+                  "last_pred_date.$": "$.last_pred_date",
+                  "season_start.$": "$.season_start"
+                }
+              },
+              "End": true
+            }
+          }
+        },
+        {
+          "StartAt": "NewGamesChoice",
+          "States": {
+            "NewGamesChoice":{
+              "Type": "Choice",
+              "Choices": [
+                {
+                  "And": [
+                    {
+                      "Variable": "$.next_game_date",
+                      "IsNull": false
+                    },
+                    {
+                      "Variable": "$.next_game_date",
+                      "StringGreaterThanPath": "$.last_pred_date"
+                    }
+                  ],
+                  "Next": "ModelInference"
+                }
+              ],
+              "Default": "Finish"
+            },
+            "ModelInference": {
+              "Type": "Pass",
+              "Next": "PredictGames"
+            },
+            "PredictGames": {
+              "Type": "Map",
+              "Next": "CreateNewRecord",
+              "Iterator": {
+                "StartAt": "PredictGame",
+                "States": {
+                  "PredictGame": {
+                    "Type": "Pass",
+                    "End": true
+                  }
+                }
+              }
+            },
+            "CreateNewRecord": {
+              "Type": "Pass",
+              "Next": "Finish"
+            },
+            "Finish": {
+              "Type": "Succeed"
+            }
+          }
         }
-      },
-      "Next": "Finish"
-    },
-    "Finish": {
-      "Type": "Succeed"
+      ]
     }
   }
 }
