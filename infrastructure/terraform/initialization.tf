beea64a993347868bede70a8453827b520afc620
Everett Robinson <evjrob@gmail.com>
2021-12-05 12:01:02 -0700

    Always build the docker image

--- infrastructure/terraform/initialization.tf
+++ infrastructure/terraform/initialization.tf
@@ -2,6 +2,11 @@ resource "null_resource" "ecr_docker_image" {
   provisioner "local-exec" {
     command = "/bin/bash assets/deploy_image.sh ${aws_ecr_repository.bayesbet_model_ecr.repository_url}"
   }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  
   depends_on = [
     aws_ecr_repository.bayesbet_model_ecr,
   ]
