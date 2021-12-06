165014a42bfec3446ac33ce1f9a44eda66e23102
Everett Robinson <evjrob@gmail.com>
2021-12-05 22:27:36 -0700

    Do not force destroy

--- infrastructure/terraform/s3.tf
+++ infrastructure/terraform/s3.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "bayesbet_web_bucket" {
   bucket = "${var.project}-web-${var.env}"
   acl    = "public-read"
-  force_destroy = true
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
@@ -11,5 +10,4 @@ resource "aws_s3_bucket" "bayesbet_web_bucket" {
 
 resource "aws_s3_bucket" "bayesbet_pipeline_bucket" {
   bucket = "${var.project}-pipeline-${var.env}"
-  force_destroy = true
 }
