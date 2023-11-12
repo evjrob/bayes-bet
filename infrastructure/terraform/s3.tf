e1376a5f071f9c9f7ac19a86a5f6bfad05d661b1
everett <evjrob@gmail.com>
2023-11-12 09:09:20 -0600

    Update public web S3 bucket specification

--- infrastructure/terraform/s3.tf
+++ infrastructure/terraform/s3.tf
@@ -1,6 +1,36 @@
 resource "aws_s3_bucket" "bayesbet_web_bucket" {
   bucket = "${var.project}-web-${var.env}"
+}
+
+resource "aws_s3_bucket_ownership_controls" "bayesbet_web_bucket" {
+  bucket = aws_s3_bucket.bayesbet_web_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bayesbet_web_bucket" {
+  bucket = aws_s3_bucket.bayesbet_web_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bayesbet_web_bucket" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bayesbet_web_bucket,
+    aws_s3_bucket_public_access_block.bayesbet_web_bucket,
+  ]
+
+  bucket = aws_s3_bucket.bayesbet_web_bucket.id
   acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "bayesbet_web_bucket" {
+  bucket = aws_s3_bucket.bayesbet_web_bucket.id
+
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
