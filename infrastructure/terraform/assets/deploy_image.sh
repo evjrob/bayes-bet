62fe0b6533aa6a8c32c9de13af60b8950a71ec6c
everett <evjrob@gmail.com>
2023-11-18 22:44:36 -0600

    Update deployment script too

--- infrastructure/terraform/assets/deploy_image.sh
+++ infrastructure/terraform/assets/deploy_image.sh
@@ -4,16 +4,19 @@
 
 # Get ECR URL components
 ecr_full_url=$1
+ecr_image_tag=$2
 ecr_image_name=${ecr_full_url#*/}
 ecr_base_url=${ecr_full_url%"$ecr_image_name"}
 echo "ECR FULL URL: $ecr_full_url"
 echo "ECR BASE URL: $ecr_base_url"
 echo "ECR IMAGE NAME: $ecr_image_name"
+echo "ECR IMAGE TAG: $ecr_image_tag"
 
 # Build the image
 docker build -t $ecr_image_name -f ../../model/Dockerfile ../../model
 
 # Push it to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ecr_base_url
-docker tag $ecr_image_name:latest $ecr_full_url:latest
-docker push $ecr_full_url:latest
+docker tag $ecr_image_name:$ecr_image_tag $ecr_full_url:$ecr_image_tag
+docker push $ecr_full_url:$ecr_image_tag
+```
