080f77c6f8fb3fff48b17ed3f1a7a076451644b3
everett <evjrob@gmail.com>
2023-12-16 16:39:39 -0600

    Add preprocess data stage

---
+++ model/experiments/generate_dataset/stages/preprocess_data.py
@@ -0,0 +1,25 @@
+import gzip
+import json
+import os
+import pandas as pd
+from tqdm import tqdm
+
+from bayesbet.nhl.stats_api import extract_game_data
+
+
+def main():
+    with gzip.open('../data/raw/games.json.gz', 'rb') as f:
+        games_json = json.loads(f.read().decode('utf-8'))
+
+    games_data_frames = []
+    for game_json in tqdm(games_json):
+        game_data_frame, _ = extract_game_data(game_json)
+        games_data_frames.append(game_data_frame)
+    
+    games = pd.concat(games_data_frames, ignore_index=True)
+    os.makedirs("../data/preprocessed", exist_ok=True)
+    games.to_parquet("../data/preprocessed/games.parquet")
+    
+
+if __name__ == "__main__":
+    main()
