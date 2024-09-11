d05b5e871628bcf337ecf49dabe9f24cf4abc545
everett <evjrob@gmail.com>
2024-09-10 22:36:51 -0500

    Add play by play data processing for traditional expected goals model

--- model/bayesbet/nhl/data_utils.py
+++ model/bayesbet/nhl/data_utils.py
@@ -135,3 +135,91 @@ def extract_game_data(games_json):
     return games, date_metadata
 
 
+def extract_shot_data(play_by_play_json):
+    home_team_id = play_by_play_json["homeTeam"]["id"]
+    goal_x_distance = 89
+    goal_y = 0
+    last_penalty_time_seconds = 0
+    shot_data = []
+
+    plays = play_by_play_json["plays"]
+    for previous_play, current_play in zip(plays[:-1], plays[1:]):
+        if current_play["typeDescKey"] not in ["shot-on-goal", "goal"]:
+            continue
+        elif current_play["typeDescKey"] == "penalty":
+            last_penalty_period = current_play["periodDescriptor"]["number"]
+            last_penalty_time = current_play["timeInPeriod"]
+            last_penalty_time_seconds = (last_penalty_period - 1) * 20 * 60 + int(last_penalty_time.split(":")[0]) * 60 + int(last_penalty_time.split(":")[1])
+            continue
+
+        goal = current_play["typeDescKey"] == "goal"
+        shot_period = current_play["periodDescriptor"]["number"]
+        shot_time = current_play["timeInPeriod"]
+        shot_time_seconds = (shot_period - 1) * 20 * 60 + int(shot_time.split(":")[0]) * 60 + int(shot_time.split(":")[1])
+        shot_detail = current_play["details"]
+        is_home_team = shot_detail["eventOwnerTeamId"] == home_team_id
+        home_team_defending_side = current_play["homeTeamDefendingSide"]
+        if home_team_defending_side == "left":
+            if is_home_team:
+                goal_x = goal_x_distance
+            else:
+                goal_x = -goal_x_distance
+        elif home_team_defending_side == "right":
+            if is_home_team:
+                goal_x = -goal_x_distance
+            else:
+                goal_x = goal_x_distance
+        shot_type = shot_detail["shotType"]
+        shot_x = shot_detail["xCoord"]
+        shot_y = shot_detail["yCoord"]
+        shot_distance = ((shot_x - goal_x) ** 2 + (shot_y - goal_y) ** 2) ** 0.5
+        shot_angle = (shot_y - goal_y) / max(abs(shot_x - goal_x), 0.1)
+        if goal_x < 0:
+            shot_angle = -shot_angle
+        last_event = previous_play["typeDescKey"]
+        last_event_detail = previous_play["details"]
+        if "xCoord" in last_event_detail and "yCoord" in last_event_detail:
+            last_event_x = last_event_detail["xCoord"]
+            last_event_y = last_event_detail["yCoord"]
+        else:
+            last_event_x = 0
+            last_event_y = 0
+        last_event_distance = ((last_event_x - shot_x) ** 2 + (last_event_y - shot_y) ** 2) ** 0.5
+        last_event_period = previous_play["periodDescriptor"]["number"]
+        last_event_time = previous_play["timeInPeriod"]
+        last_event_time_seconds = (last_event_period - 1) * 20 * 60 + int(last_event_time.split(":")[0]) * 60 + int(last_event_time.split(":")[1])
+        time_since_last_event = shot_time_seconds - last_event_time_seconds
+        situation_code = [int(d) for d in str(current_play["situationCode"])]
+        away_goalie = situation_code[0]
+        away_skaters = situation_code[1]
+        home_skaters = situation_code[2]
+        home_goalie = situation_code[3]
+        if home_skaters != away_skaters and (home_skaters < 5 or away_skaters < 5):
+            powerplay_duration = shot_time_seconds - last_penalty_time_seconds
+        else:
+            powerplay_duration = 0
+        opposing_skaters = away_skaters if is_home_team else home_skaters
+        current_skaters = home_skaters if is_home_team else away_skaters
+        empty_net = (is_home_team and away_goalie == 0) or (not is_home_team and home_goalie == 0)
+        
+        row = {
+            "shot_type": shot_type,
+            "shot_x": shot_x,
+            "shot_y": shot_y,
+            "shot_distance": shot_distance,
+            "shot_angle": shot_angle,
+            "last_event": last_event,
+            "last_event_x": last_event_x,
+            "last_event_y": last_event_y,
+            "last_event_distance": last_event_distance,
+            "time_since_last_event": time_since_last_event,
+            "opposing_skaters": opposing_skaters,
+            "current_skaters": current_skaters,
+            "powerplay_duration": powerplay_duration,
+            "empty_net": empty_net,
+            "goal": goal,
+        }
+
+        shot_data.append(row)
+        shot_data = pd.DataFrame(shot_data)
+        return shot_data
--- model/experiments/generate_dataset/stages/preprocess_data.py
+++ model/experiments/generate_dataset/stages/preprocess_data.py
@@ -5,12 +5,15 @@ import pandas as pd
 import sys
 from tqdm import tqdm
 
-from bayesbet.nhl.data_utils import extract_game_data
+from bayesbet.nhl.data_utils import extract_game_data, extract_shot_data
 
 
 def main(season):
     with gzip.open(f'../data/raw/{season}/games.json.gz', 'rb') as f:
         games_json = json.loads(f.read().decode('utf-8'))
+
+    with gzip.open(f'../data/raw/{season}/play_by_play.json.gz', 'rb') as f:
+        play_by_play_json = json.loads(f.read().decode('utf-8'))
 
     games_data_frames = []
     for game_json in tqdm(games_json):
@@ -21,9 +24,18 @@ def main(season):
     games = pd.concat(games_data_frames, ignore_index=True)
     games = games[~games["game_type"].isin(["Pr", "A", "Other"])]
     games = games.reset_index(drop=True)
+
+    shots_data_frames = []
+    for game_id in games["game_pk"]:
+        game_play_by_play_json = play_by_play_json[str(game_id)]
+        shot_data_frame = extract_shot_data(game_play_by_play_json)
+        shots_data_frames.append(shot_data_frame)
+
+    shots = pd.concat(shots_data_frames, ignore_index=True)
     
     os.makedirs(f"../data/preprocessed/{season}", exist_ok=True)
     games.to_parquet(f"../data/preprocessed/{season}/games.parquet")
+    shots.to_parquet(f"../data/preprocessed/{season}/shots.parquet")
     
 
 if __name__ == "__main__":
